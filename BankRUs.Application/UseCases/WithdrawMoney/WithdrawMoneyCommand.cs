d10751f430d399d21f570ffcc846790e8b062766
Palina Boczkowska <palina.boczkowska@gmail.com>
2026-02-12 10:01:12 +0100

    Implement withdrawal feature

---
+++ BankRUs.Application/UseCases/WithdrawMoney/WithdrawMoneyCommand.cs
@@ -0,0 +1,8 @@
+﻿namespace BankRUs.Application.UseCases.WithdrawMoney;
+
+public sealed record WithdrawMoneyCommand(
+    string UserId,
+    Guid BankAccountId,
+    decimal Amount,
+    string? Reference
+);
